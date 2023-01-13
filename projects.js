c541cd4c2c3fdd1ce812ac9778c8f9e5ffcf015c
Damian Pabian <anesticnei@gmail.com>
2023-01-13 20:58:20 +0100

    init

---
+++ projects.js
@@ -0,0 +1,196 @@
+const projects = [
+    {
+        id: 0,
+        id_number: 0,
+        id_name: "project-1",
+        id_box: 'project-box-id-1',
+        span_id: 'span-project-1',
+        // class_for_each_project: 
+
+        class_name_one: "project-one-box-class-one",
+        class_name_two: "project-one-box-class-two",
+
+        name: "Music",
+        sub_name: "Web",
+        text_project: "to get more info about this project",
+        text_project_open: "Siemka to jest 1 text",
+
+        button: "click here",
+        button_open: "close here",
+        button_close: "close here",
+        class_img: "open-project-img img-1",
+
+        image: "./images/music-img.png",
+
+        class_button_project: "project-one",
+        class_text_project: "project-one-text",
+
+        modal_text: "HEJ WATS UP HEJ WATS UP HEJ WATS UP HEJ WATS UP",
+
+        // background_color: "#fff",
+
+
+
+        id_project_container: 'project-one-container',
+
+        data_for_tab: '1',
+
+        grid_project_box: 'grid-project-box-one',
+
+        btn_for_all: 'btn-for-all',
+    },
+    {
+        id: 1,
+        id_number: 1,
+        id_name: "project-2",
+        id_box: 'project-box-id-2',
+        span_id: 'span-project-2',
+        
+        class_name_one: "project-two-box-class-one",
+        class_name_two: "project-two-box-class-two",
+        
+        name: "Photography",
+        sub_name: "Web",
+        text_project: "to get more info about this project",
+
+        button: "click here",
+        button_close: "close here",
+        image: "./images/photography-img.jpg",
+
+        class_button_project: "project-two",
+        class_text_project: "project-two-text",
+
+        modal_text: "HEJ WATS UP HEJ WATS UP HEJ WATS UP HEJ WATS UP",
+
+        id_project_container: 'project-two-container',
+        data_for_tab: '2',
+
+        grid_project_box: 'grid-project-box-two',
+        btn_for_all: 'btn-for-all',
+    },
+    {
+        id: 2,
+        id_number: 2,
+        id_name: "project-3",
+        id_box: 'project-box-id-3',
+        span_id: 'span-project-3',
+
+        class_name_one: "project-three-box-class-one",
+        class_name_two: "project-three-box-class-two",
+
+        name: "Music",
+        sub_name: "Web",
+        text_project: "to get more info about this project",
+
+        button: "click here",
+        button_close: "close here",
+        image: "./images/project_3.jpg",
+
+        class_button_project: "project-three",
+        class_text_project: "project-three-text",
+
+        modal_text: "HEJ WATS UP HEJ WATS UP HEJ WATS UP HEJ WATS UP",
+
+        id_project_container: 'project-three-container' ,
+
+        data_for_tab: '3',
+        grid_project_box: 'grid-project-box-three',
+        open_project_text: "No Witam Widam w Obiekcie",
+        btn_for_all: 'btn-for-all',
+    },
+    {
+        id: 3,
+        id_number: 3,
+        id_name: "project-4",
+        id_box: 'project-box-id-4',
+        span_id: 'span-project-4',
+
+        class_name_one: "project-three-box-class-one",
+        class_name_two: "project-three-box-class-two",
+
+        name: "In",
+        sub_name: "Progress",
+        text_project: "In Progress",
+
+        button: "click here",
+        button_close: "close here",
+        image: "./images/project_3.jpg",
+
+        class_button_project: "project-three",
+        class_text_project: "project-three-text",
+
+        modal_text: "HEJ WATS UP HEJ WATS UP HEJ WATS UP HEJ WATS UP",
+
+        span_id: 'span-project-three',
+        id_project_container: 'project-three-container' ,
+
+        data_for_tab: '3',
+        grid_project_box: 'grid-project-box-three',
+        open_project_text: "No Witam Widam w Obiekcie",
+        btn_for_all: 'btn-for-all',
+
+    },
+    {
+        id: 4,
+        id_number: 4,
+        id_name: "project-5",
+        id_box: 'project-box-id-5',
+        span_id: 'span-project-5',
+
+        class_name_one: "project-three-box-class-one",
+        class_name_two: "project-three-box-class-two",
+
+        name: "In",
+        sub_name: "Progress",
+        text_project: "In Progress",
+
+        button: "click here",
+        button_close: "close here",
+        image: "./images/background-hero.jpg",
+
+        class_button_project: "project-three",
+        class_text_project: "project-three-text",
+
+        modal_text: "HEJ WATS UP HEJ WATS UP HEJ WATS UP HEJ WATS UP",
+
+        span_id: 'span-project-three',
+        id_project_container: 'project-three-container' ,
+
+        data_for_tab: '3',
+        grid_project_box: 'grid-project-box-three',
+        open_project_text: "No Witam Widam w Obiekcie",
+        btn_for_all: 'btn-for-all',
+    },
+    {
+        id: 5,
+        id_number: 5,
+        id_name: "project-6",
+        id_box: 'project-box-id-6',
+        span_id: 'span-project-6',
+
+        class_name_one: "project-three-box-class-one",
+        class_name_two: "project-three-box-class-two",
+
+        name: "In",
+        sub_name: "Progress",
+        text_project: "In Progress",
+
+        button: "click here",
+        button_close: "close here",
+        image: "./images/background-hero-2.jpg",
+
+        class_button_project: "project-three",
+        class_text_project: "project-three-text",
+
+        modal_text: "HEJ WATS UP HEJ WATS UP HEJ WATS UP HEJ WATS UP",
+
+        span_id: 'span-project-three',
+        id_project_container: 'project-three-container' ,
+
+        data_for_tab: '3',
+        grid_project_box: 'grid-project-box-three',
+        open_project_text: "No Witam Widam w Obiekcie",
+        btn_for_all: 'btn-for-all',
+    },
+    
+]
